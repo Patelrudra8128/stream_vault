c78f5d6d0c9c389e08c8fa651952882666775bee
Rudra Patel <126685264+Patelrudra8128@users.noreply.github.com>
2026-06-10 12:45:05 +0530

    Added videos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -176,7 +176,7 @@ function HomeContent() {
       )}
 
       {/* Video Grid */}
-      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
         {filteredVideos.map((video) => (
           <VideoCard key={video.id} video={video} />
         ))}
